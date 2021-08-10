75111f27d9581deaec2dac332f3b4d231b208d03
DerzoGit <journetalexandre.contact@gmail.com>
2021-08-10 16:59:06 +0200

    recup

---
+++ frontend/js/index.js
@@ -0,0 +1,35 @@
+const getProducts = async () => {
+    const response = await fetch("http://localhost:3000/api/teddies");
+    const teddies = await response.json();
+    return teddies;
+};
+
+
+
+const displayProducts = async () => {
+
+    let teddies = [];
+    try {
+        teddies = await getProducts();
+        console.log(teddies);
+        const template = document.querySelector("#teddyTemplate");
+        for (let teddy of teddies) {
+            const ficheProduct = document.querySelector(".ficheProduct");
+            const clone = document.importNode(template.content, true);
+            const teddyName = clone.querySelector(".card-title");
+            teddyName.innerHTML = teddy.name;
+            const teddyImg = clone.querySelector(".card-img-top");
+            teddyImg.src = teddy.imageUrl;
+            const teddyLink = clone.querySelector(".card-link");
+            teddyLink.href = `product.html?id=${teddy._id}`;
+            ficheProduct.appendChild(clone);
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+    
+}
+
+displayProducts();
