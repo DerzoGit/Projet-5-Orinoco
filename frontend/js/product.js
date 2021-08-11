b3eb26b37c0eac92fcb24b23e135ca2d0c867832
DerzoGit <journetalexandre.contact@gmail.com>
2021-08-12 00:28:43 +0200

    add colors, desc product

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -2,31 +2,45 @@ const params = (new URL(document.location)).searchParams;
 const id = params.get("id");
 console.log(id);
 
-const getProducts = async () => {
+const getProductId = async () => {
     const response = await fetch(`http://localhost:3000/api/teddies/${id}`);
     console.log(response);
-    const teddies = await response.json();
-    return teddies;
+    const teddy = await response.json();
+    return teddy;
 };
 
-const displayProducts = async () => {
+const displayProduct = async () => {
 
-    let teddies = [];
     try {
-        teddies = await getProducts();
-        console.log(teddies);
-        const template = document.querySelector("#teddyList");
-        for (let teddy of teddies) {
+        let teddy = await getProductId();
+        console.log(teddy);
+        const template = document.querySelector("#teddyProduct");
+        
             const productCard = document.querySelector(".productCard");
             const clone = document.importNode(template.content, true);
+
             const teddyName = clone.querySelector(".card-title");
             teddyName.innerHTML = teddy.name;
+
             const teddyImg = clone.querySelector(".card-img-top");
             teddyImg.src = teddy.imageUrl;
+
+            const teddyDescription = clone.querySelector(".teddy-description");
+            teddyDescription.innerHTML = teddy.description;
+
+            const teddyPrice = clone.querySelector(".teddy-price");
+            teddyPrice.innerHTML = teddy.price / 100 + "€";
+
+            for (i = 0; i < teddy.colors.length; i++) {
+                const teddyColors = clone.querySelector(".teddy-color");
+                teddyColors.innerHTML = teddy.colors;
+            }
+
             const teddyLink = clone.querySelector(".card-link");
             teddyLink.href = `/frontend/html/product.html?id=${teddy._id}`;
+
             productCard.appendChild(clone);
-        }
+        
         
     } catch (error) {
         console.log(error);
@@ -35,4 +49,4 @@ const displayProducts = async () => {
     
 }
 
-displayProducts();
+displayProduct();
