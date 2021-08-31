249984a3d1b32eca12f08707c437bb3f5de17df5
DerzoGit <journetalexandre.contact@gmail.com>
2021-08-31 12:35:22 +0200

    add total price & to localStorage

--- frontend/js/panier.js
+++ frontend/js/panier.js
@@ -31,10 +31,10 @@ const displayProduct = async () => {
             teddyQuantity.innerHTML = teddy.quantity;
 
             const teddyPrice = clone.querySelector(".teddy-price");
-            teddyPrice.innerHTML = teddy.price / 100 + "€";
+            teddyPrice.innerHTML = new Intl.NumberFormat("fr-FR", {style: "currency", currency:"EUR"}).format(teddy.price/100);
 
             const teddyTotalPrice = clone.querySelector(".teddy-total-price");
-            teddyTotalPrice.innerHTML = (teddy.price * teddy.quantity) /100 + "€" ;
+            teddyTotalPrice.innerHTML = new Intl.NumberFormat("fr-FR", {style: "currency", currency:"EUR"}).format(teddy.price*teddy.quantity/100);
 
             basketList.appendChild(clone);
         }
@@ -44,5 +44,28 @@ const displayProduct = async () => {
     }
 
 }
+
+const totalPrice = async () => {
+    const calculPrice = [];
+    
+    console.log(calculPrice);
+    for (let teddy of choosenTeddies) {
+        let itemPrice = teddy.price * teddy.quantity;
+        calculPrice.push(itemPrice);
+    }
+    console.log(calculPrice);
+    // Récupère le prix total des produits du localStorage 
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const totalPrice = calculPrice.reduce(reducer, 0) / 100;
+    console.log(totalPrice);
+
+    // Ajout du prix total au localStorage
+    localStorage.setItem("basketPrice", totalPrice);
+    
+    const basketTotalPrice = document.querySelector(".basket--total-price")
+    basketTotalPrice.innerHTML = new Intl.NumberFormat("fr-FR", {style: "currency", currency:"EUR"}).format(totalPrice);
+
+}
+totalPrice();
 
 displayProduct();
